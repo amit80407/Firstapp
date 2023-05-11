92eb467a58f292583727c1741d28d791f62ac4ec
Amitkumar <amitkumar80407@gmail.com>
2023-05-12 00:54:43 +0530

    Initial commit

---
+++ TelerikAspNetCoreApp1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikAspNetCoreApp1.Models;
+
+namespace TelerikAspNetCoreApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+        public IActionResult student()
+        {
+            List<Student> students = Student.getstudent();
+            return View(students);
+        }
+    }
+}
---
+++ TelerikAspNetCoreApp1/Models/Student.cs
@@ -0,0 +1,53 @@
+﻿using Npgsql;
+
+namespace TelerikAspNetCoreApp1.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string  course { get; set; }
+        public string  bracnh { get; set; }
+
+        public static List<Student> getstudent()
+        {
+            List<Student> students = new List<Student>();
+            NpgsqlConnection con = new NpgsqlConnection(connection());
+            con.Open();
+            NpgsqlCommand cm = new NpgsqlCommand("select * from student", con);
+            NpgsqlDataReader dr =cm.ExecuteReader();    
+                while(dr.Read()) {
+                Student st = new Student();
+                st.id = dr.GetInt32(0);
+                st.name = dr.GetString(1);
+                st.course = dr.GetString(2);
+                st.bracnh = dr.GetString(3);
+                students.Add(st);
+            }
+
+
+            return students;
+        {
+
+        }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static string connection()
+        {
+            return "user id=postgres;password=amit8888;host=localhost;port=5432;database=Test; ";
+        }
+
+    }
+}
